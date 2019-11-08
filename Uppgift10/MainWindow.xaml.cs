9d1e2a8186ca84c0bad0641f58e967e51217191a
Olivia Björner <filipokus.130@hotmail.com>
2019-11-08 13:59:16 +0100

    Uppgift 10 är påbörjad

---
+++ Uppgift10/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRandomNumber_Click(object sender, RoutedEventArgs e)
+        {
+            Random slump = new Random();
+            int slumptal = slump.Next(500);
+            if (slumptal < 0)
+            {
+                btnGuess.IsEnabled = false;
+            }
+            else
+            {
+                btnGuess.IsEnabled = true;
+            }
+            txbGuess.Text = "Skriv in din gissning! Det slumpade talet är mellan 0-1000.";
+        }
+
+        private void btnGuess_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
