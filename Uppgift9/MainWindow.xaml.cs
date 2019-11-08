2ce48f3394eec68610b9b7c000821d1124e7d773
Olivia Björner <filipokus.130@hotmail.com>
2019-11-08 13:17:07 +0100

    Uppgift 9a+b - Klar

--- Uppgift9/MainWindow.xaml.cs
+++ Uppgift9/MainWindow.xaml.cs
@@ -29,22 +29,45 @@ namespace Uppgift9
         {
             string name = txtName.Text;
             int age = int.Parse(txtAge.Text);
-            if (age < 7)
+            if (age < 15 && rbtnWithAdult.IsChecked == false && rbtnWithoutAdult.IsChecked == false)
             {
-                //Barntillåten + 7 årsgräns i vuxet sällskap
+                //Felmeddelande
+                txbInfo.Text = "Du måste fylla i om du går tillsamans med en vuxen eller ej för att få ett korrekt svar.";
+            }
+            else if (age < 7 && rbtnWithoutAdult.IsChecked == true)
+            {
+                //Barntillåten
+                txbInfo.Text = $"Hej {name}, eftersom att du är {age} år gammal kan du se barntillåtna filmer.";
+            }
+            else if (age < 7 && rbtnWithAdult.IsChecked == true)
+            {
+                //Barntillåten + 7-årsgräns
                 txbInfo.Text = $"Hej {name}, eftersom att du är {age} år gammal kan du se barntillåtna filmer. " +
-                    $"Går du tillsammans med en vuxen får du även se filmer med 7-årsgräns.";
+                    $"Eftersom du går tillsammans med en vuxen får du även se filmer med 7-årsgräns.";
+
             }
-           else if (age >= 7 && age < 11)
+           else if (age >= 7 && age < 11 && rbtnWithoutAdult.IsChecked == true)
             {
-                //Får se filmer med åldersgräns 7 & barntillåtna filmer + 11 årsgräns i vuxet sällskap
+                //Får se filmer med åldersgräns 7 & barntillåtna filmer
                 txbInfo.Text = $"Hej {name}, eftersom att du är {age} år gammal kan du se på filmer med 7-årsgräns. " +
-                    $"Går du tillsammans med en vuxen får du även se filmer med 11-årsgräns. " +
-                    $"Givetvis får du också titta på barntillåtna filmer utan vuxen.";
+                    $"Givetvis får du även titta på barntillåtna filmer utan vuxen.";
             }
-            else if (age >=11 && age < 15)
+            else if (age >= 7 && age < 11 && rbtnWithAdult.IsChecked == true)
+            {
+                //Åldersgräns 7 & barntillåtna filmer + 11 årsgräns
+                txbInfo.Text = $"Hej {name}, eftersom att du är {age} år gammal kan du se på filmer med 7-årsgräns. " +
+                    $"Eftersom du går tillsammans med en vuxen får du även se filmer med 11-årsgräns. " +
+                    $"Givetvis får ni också titta på barntillåtna filmer.";
+            }
+            else if (age >=11 && age < 15 && rbtnWithoutAdult.IsChecked == true)
             {
                 //Får se filmer med åldersgräns 11, 7 & barntillåtna filmer
+                txbInfo.Text = $"Hej {name}, eftersom att du är {age} år gammal kan du se på filmer med 11-årsgräns. " +
+                    $"Givetvis får du också titta på barntillåtna filmer eller filmer med 7-årsgräns.";
+            }
+            else if (age >= 11 && age < 15 && rbtnWithAdult.IsChecked == true)
+            {
+                //Får se filmer med åldersgräns 11, 7 & barntillåtna filmer oavsett vuxet sällskap
                 txbInfo.Text = $"Hej {name}, eftersom att du är {age} år gammal kan du se på filmer med 11-årsgräns. " +
                     $"Det spelar ingen roll om du går tillsamans med en vuxen. " +
                     $"Givetvis får du också titta på barntillåtna filmer eller filmer med 7-årsgräns.";
