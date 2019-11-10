e81f0a5d2f6f3dd682392555ea841055c36f9533
Olivia Björner <filipokus.130@hotmail.com>
2019-11-10 22:51:32 +0100

    Hmmm... En del kod verkar inte fungera men jag sparar den ändå för ikväll

--- Uppgift10/MainWindow.xaml.cs
+++ Uppgift10/MainWindow.xaml.cs
@@ -24,25 +24,50 @@ namespace Uppgift10
         {
             InitializeComponent();
         }
-
+        Random slump = new Random();
         private void btnRandomNumber_Click(object sender, RoutedEventArgs e)
         {
-            Random slump = new Random();
-            int slumptal = slump.Next(500);
-            if (slumptal < 0)
-            {
-                btnGuess.IsEnabled = false;
-            }
-            else
+            int slumptal = slump.Next(1001);
+            if (slumptal >= 0)
             {
                 btnGuess.IsEnabled = true;
             }
             txbGuess.Text = "Skriv in din gissning! Det slumpade talet är mellan 0-1000.";
+            lblRandomNumber.Content = slumptal;
         }
 
         private void btnGuess_Click(object sender, RoutedEventArgs e)
         {
-
+            int guess = int.Parse(txtGuess.Text);
+            string randomNumber = lblRandomNumber.Content.ToString();
+            int slumptal = int.Parse(randomNumber);
+            //int numberOfTries = btnGuess;
+            if (guess == slumptal)
+            {
+                txbGuess.Text = $"Grattis du gissade {slumptal}, vilket var rätt på !";
+            }
+            if (guess < slumptal - 100)
+            {
+                if (guess < slumptal && guess > slumptal - 100 )
+                {
+                    txbGuess.Text = $"Nämen! Det slumpade talet är bara lite högre än {guess}!";
+                }
+                else
+                {
+                    txbGuess.Text = $"OJOJ! Det slumpade talet är mycket högre än {guess}!";
+                }
+            }
+            if (guess > slumptal + 100)
+            {
+                if (guess > slumptal && guess < slumptal + 100)
+                {
+                    txbGuess.Text = $"Nämen! Det slumpade talet är bara lite lägre än {guess}!";
+                }
+                else
+                {
+                    txbGuess.Text = $"OJOJ! Det slumpade talet är mycket lägre än {guess}!";
+                }
+            }
         }
     }
 }
