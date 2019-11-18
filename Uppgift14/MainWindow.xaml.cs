79aa82d07262b6f39dc3a79b7f10ecf6c2f1f9fc
Olivia Björner <filipokus.130@hotmail.com>
2019-11-18 21:12:54 +0100

    Uppgift 14 - Klar

---
+++ Uppgift14/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        ProgramLogic programLogic = new ProgramLogic();
+        private void btnCalculateAge_Click(object sender, RoutedEventArgs e)
+        {
+            string birthYear = txtBirthYear.Text;
+            bool IsBirthYearValid = programLogic.IsInputOk(birthYear);
+            if (IsBirthYearValid == true)
+            {
+                int age = programLogic.CalculateAge(birthYear);
+                MessageBox.Show($"Du är {age} år gammal");
+            }
+            else
+            {
+                MessageBox.Show($"Du måste mata in enbart siffror");
+            }
+        }
+    }
+}
---
+++ Uppgift14/ProgramLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift14
+{
+    class ProgramLogic
+    {
+        private int year = DateTime.Now.Year;
+        public bool IsInputOk(string birthYear)
+        {
+            foreach (char i in birthYear)
+            {
+                if (!char.IsDigit(i))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public int CalculateAge(string birthYear)
+        {
+            int age = year - int.Parse(birthYear);
+            return age;
+        }
+    }
+}
