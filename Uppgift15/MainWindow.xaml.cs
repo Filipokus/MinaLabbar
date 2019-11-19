415f79a3b6c5dbd6616e38a1d61060c1f5335d8a
Olivia Björner <filipokus.130@hotmail.com>
2019-11-19 01:25:42 +0100

    Uppgift 15 - Rotvälskan fungerar inte än men är på Gcresten är klart

---
+++ Uppgift15/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        ProgramLogic programLogic = new ProgramLogic();
+        private void BtnTranslate_Click(object sender, RoutedEventArgs e)
+        {
+            string textToTranslate = txtOriginal.Text;
+          
+            int NumberOfVowels = programLogic.NumberOfVowels(textToTranslate);
+            lblNumberOfVowels.Content = $"Antal vokaler: {NumberOfVowels}";
+
+           
+            string translatedText = programLogic.Jibberish(textToTranslate);
+            txbTranslation.Text = translatedText;
+        }
+    }
+}
---
+++ Uppgift15/ProgramLogic.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift15
+{
+    class ProgramLogic
+    {
+        private string[] lowerCaseVowels = new string[] { "a", "e", "i", "o", "u", "y", "å", "ä", "ö" };
+        private string[] upperCaseVowels = new string[] { "A", "E", "I", "O", "U", "Y", "Å", "Ä", "Ö",  };
+        public int NumberOfVowels(string textToTranslate) 
+        {
+            int vowelCount = 0;
+            foreach (char letter in textToTranslate)
+            {
+                if (char.Parse(lowerCaseVowels[0]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(lowerCaseVowels[1]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(lowerCaseVowels[2]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(lowerCaseVowels[3]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(lowerCaseVowels[4]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(lowerCaseVowels[5]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(lowerCaseVowels[6]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(lowerCaseVowels[7]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(lowerCaseVowels[8]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(upperCaseVowels[0]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(upperCaseVowels[1]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(upperCaseVowels[2]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(upperCaseVowels[3]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(upperCaseVowels[4]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(upperCaseVowels[5]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(upperCaseVowels[6]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(upperCaseVowels[7]) == letter)
+                {
+                    vowelCount++;
+                }
+                else if (char.Parse(upperCaseVowels[8]) == letter)
+                {
+                    vowelCount++;
+                }
+            }
+            return vowelCount;
+        }
+        public bool IsLowerCaseVowel(char charToTest)
+        {
+            if (charToTest == char.Parse(lowerCaseVowels[0]) || charToTest == char.Parse(lowerCaseVowels[1]))
+            {
+                return true;
+            }
+            else if (charToTest == char.Parse(lowerCaseVowels[2]) || charToTest == char.Parse(lowerCaseVowels[3]))
+            {
+                return true;
+            }
+            else if (charToTest == char.Parse(lowerCaseVowels[4]) || charToTest == char.Parse(lowerCaseVowels[5]))
+            {
+                return true;
+            }
+            else if (charToTest == char.Parse(lowerCaseVowels[6]) || charToTest == char.Parse(lowerCaseVowels[7]) || charToTest == char.Parse(lowerCaseVowels[8]))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool IsUpperCaseVowel(char charToTest)
+        {
+            if (charToTest == char.Parse(upperCaseVowels[0]) || charToTest == char.Parse(upperCaseVowels[1]))
+            {
+                return true;
+            }
+            else if (charToTest == char.Parse(upperCaseVowels[2]) || charToTest == char.Parse(upperCaseVowels[3]))
+            {
+                return true;
+            }
+            else if (charToTest == char.Parse(upperCaseVowels[4]) || charToTest == char.Parse(upperCaseVowels[5]))
+            {
+                return true;
+            }
+            else if (charToTest == char.Parse(upperCaseVowels[6]) || charToTest == char.Parse(upperCaseVowels[7]) || charToTest == char.Parse(upperCaseVowels[8]))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string Jibberish(string textToTranslate)
+        {
+            string translatedText = null;
+            translatedText = textToTranslate;
+            foreach (char letter in textToTranslate)
+            {
+                if (IsLowerCaseVowel(letter) == true)
+                {
+                   translatedText.Replace(letter, char.Parse("ö"));
+                }
+                else if (IsUpperCaseVowel(letter) == true)
+                {
+                   translatedText.Replace(letter, char.Parse("Ö"));
+                }
+                //else
+                //{
+                //    translatedText[] = textToTranslate[letter];
+                //}
+            }
+            return translatedText;
+        }
+
+    }
+}
